a1edb9bea37307c2b2f3e414975b5deb4e5d9d49
Ricardo <dones.bugan@gmail.com>
2020-07-16 10:04:04 -0300

    projeto inicial

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./App.css";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import 'fontsource-roboto';
+
+import {Container, Typography } from "@material-ui/core"
+class App extends Component {
+  render() {
+    return (
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" component="h1" align="center" >Formulário de cadastro</Typography>
+        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
+      </Container>
+    );
+  }
+}
+
+function aoEnviarForm(dados){
+  console.log(dados);
+}
+
+function validarCPF(cpf){
+  if(cpf.length !== 11){
+    return {valido:false, texto:"CPF deve ter 11 digitos."}
+  }else{
+    return {valido:true, texto:""}
+  }
+}
+
+export default App;
