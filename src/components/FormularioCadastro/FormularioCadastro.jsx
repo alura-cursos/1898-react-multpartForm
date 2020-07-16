c7520b855e7a07db82dea6d4c6ec49e98655dd58
Ricardo <dones.bugan@gmail.com>
2020-07-16 11:08:46 -0300

    video 2.1

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,14 +3,27 @@ import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
 
+import { Typography } from "@material-ui/core";
 function FormularioCadastro({ aoEnviar, validarCPF }) {
-  return (
-    <>
-      <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-      <DadosUsuario />
-      <DadosEntrega/>
-    </>
-  );
+  const [etapaAtual, setEtapaAtual] = useState(1);
+
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuario />;
+      case 1:
+        return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />;
+      case 2:
+        return <DadosEntrega />;
+      default:
+        return <Typography>Erro ao selecionar formulário</Typography>;
+    }
+  }
+
+  return <>{formularioAtual(etapaAtual)}</>;
 }
 
+//
+//<DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>1
+// <DadosUsuario />0
 export default FormularioCadastro;
