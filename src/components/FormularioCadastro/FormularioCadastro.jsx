f608f197db902a9c1ff016bdf47114f1a4a4e7f6
Ricardo <dones.bugan@gmail.com>
2020-07-16 11:30:40 -0300

    video 2.3

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,31 +3,20 @@ import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
 
-import { Typography } from "@material-ui/core";
 function FormularioCadastro({ aoEnviar, validarCPF }) {
-  const [etapaAtual, setEtapaAtual] = useState(0);
+  const [etapaAtual, setEtapaAtual] = useState(4);
 
-  function proximo(){
-    setEtapaAtual(etapaAtual+1);
+  const formularios = [
+    <DadosUsuario aoEnviar={proximo} />,
+    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={aoEnviar} />,
+  ];
+
+  function proximo() {
+    setEtapaAtual(etapaAtual + 1);
   }
 
-  function formularioAtual(etapa) {
-    switch (etapa) {
-      case 0:
-        return <DadosUsuario aoEnviar={proximo} />;
-      case 1:
-        return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />;
-      case 2:
-        return <DadosEntrega  aoEnviar={aoEnviar}/>;
-      default:
-        return <Typography>Erro ao selecionar formulário</Typography>;
-    }
-  }
-
-  return <>{formularioAtual(etapaAtual)}</>;
+  return <>{formularios[etapaAtual]}</>;
 }
 
-//
-//<DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>1
-// <DadosUsuario />0
 export default FormularioCadastro;
